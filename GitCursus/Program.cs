cd8bad8c17f56e5c2980c84500e02ada4b3a30d2
teye <teye@skynet.be>
2017-10-02 11:12:48 +0200

    allemaal

--- GitCursus/Program.cs
+++ GitCursus/Program.cs
@@ -11,7 +11,7 @@ namespace GitCursus
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hallo iedereen");
+            Console.WriteLine("Hallo allemaal");
 
         }
     }
