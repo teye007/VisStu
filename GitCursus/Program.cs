4760046f678f2449c69d8e6105762790d21b80d3
teye <teye@skynet.be>
2017-10-02 11:04:27 +0200

    toutlemonde

--- GitCursus/Program.cs
+++ GitCursus/Program.cs
@@ -11,7 +11,7 @@ namespace GitCursus
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Bonjour");
+            Console.WriteLine("Bonjour tout le monde");
 
         }
     }
