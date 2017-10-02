b733e39a9dcc9c2b52eb23b51333ba8572b0eaee
teye007 <32390440+teye007@users.noreply.github.com>
2017-10-02 11:23:02 +0200

    Update Program.cs

--- GitCursus/Program.cs
+++ GitCursus/Program.cs
@@ -11,7 +11,7 @@ namespace GitCursus
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hallo allemaal2");
+            Console.WriteLine("Hallo everyone");
 
         }
     }
